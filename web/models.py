eb9bf8ad9ad221a0fe682a36eda7e1e351f01289
unknown <m09144192012@gmail.com>
2018-04-08 20:08:02 +0430

    kharj model is there

--- web/models.py
+++ web/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+class kharj(models.Model):
+    sharh = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    mablagh = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
