d224c15695171c824782741d938b7ec9e62fd9d6
unknown <m09144192012@gmail.com>
2018-04-09 10:21:43 +0430

    daramad add to model and biutification

--- web/admin.py
+++ web/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import kharj
+from .models import kharj,daramad
 
 # Register your models here.
 admin.site.register(kharj)
+admin.site.register(daramad)
--- web/models.py
+++ web/models.py
@@ -1,8 +1,20 @@
 from django.db import models
-
 from django.contrib.auth.models import User
+
 class kharj(models.Model):
     sharh = models.CharField(max_length=255)
     date = models.DateTimeField()
     mablagh = models.BigIntegerField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):   #dar python 2.X => def __unicode__(self):
+        return "{}---{}".format(self.date,self.mablagh)
+        #return (self.sharh)
+
+
+class daramad(models.Model):
+    sharh = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    mablagh = models.BigIntegerField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+        return "{}---{}".format(self.date,self.mablagh)
